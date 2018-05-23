570c094e86e2d85231d4bfdaa9e83f3ddebc0286
Ray Tsang <rayt@google.com>
2018-05-23 16:42:04 +0200

    updated to spring boot 2

--- 3-runtime-config/guestbook-service/src/main/java/com/example/guestbook/ServletInitializer.java
+++ 3-runtime-config/guestbook-service/src/main/java/com/example/guestbook/ServletInitializer.java
@@ -1,7 +1,7 @@
 package com.example.guestbook;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
