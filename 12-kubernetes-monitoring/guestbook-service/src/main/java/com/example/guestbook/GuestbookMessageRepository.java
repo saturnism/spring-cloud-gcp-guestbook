43c310b00065a0576fd75747c1a97846dbe0d0d9
Ray Tsang <rayt@google.com>
2018-07-18 18:35:24 -0400

    Added Lab 12

---
+++ 12-kubernetes-monitoring/guestbook-service/src/main/java/com/example/guestbook/GuestbookMessageRepository.java
@@ -0,0 +1,14 @@
+package com.example.guestbook;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface GuestbookMessageRepository extends 
+	PagingAndSortingRepository<GuestbookMessage, String> {
+
+	List<GuestbookMessage> findByName(String name);
+}
+
